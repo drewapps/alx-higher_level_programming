84b5c5d9e18286c118dba75620e4fcb5be5ef7d8
drewapps <druapps@gmail.com>
2022-07-06 00:38:10 +0300

    6-load_from_json_file.py file added

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+i#!/usr/bin/python3
+
+"""Defines a JSON file-reading function."""
+import json
+
+
+def load_from_json_file(filename):
+    """Create a Python object from a JSON file."""
+    with open(filename) as f:
+        return json.load(f)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+
